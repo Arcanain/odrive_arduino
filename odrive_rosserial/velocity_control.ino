7bc5d0c6501acece9c583b967e3d031437dca238
Ramune6110 <arduino6110@gmail.com>
2021-08-18 22:42:21 +0900

    [remove]non neccesarry functions

--- odrive_rosserial/velocity_control.ino
+++ odrive_rosserial/velocity_control.ino
@@ -1,3 +1,12 @@
+/* Board  : Arduino Mega 2560
+ * Author : Ramune6110
+ * Data   : 2021 08/18(とりあえずキャリブレーションは成功)
+ * ********************************************************************************
+ * Topic
+ * Publish  | cmd_vel
+ * Sbscribe | cmd_vel
+ * ********************************************************************************
+ */
 /* Odrive */
 //#include <ODriveTool.h>
 #include <HardwareSerial.h>
@@ -15,7 +24,6 @@
 #include <std_msgs/Empty.h>
 #include <ArduinoHardware.h>
 #include <geometry_msgs/Twist.h>
-/******/
 
 // Arduino Mega or Due - Serial1
 // pin 19: RX - connect to ODrive TX
@@ -29,91 +37,52 @@
 HardwareSerial& odrive_serial = Serial2;
 
 // ODrive object
-//ODriveTool odrive(Serial1);
+// ODriveTool odrive(Serial1);
 ODriveArduino odrive(odrive_serial);
 
 void messageCb(const geometry_msgs::Twist& msg);
 
-//void odrive_cb(const std_msgs::Float32MultiArray& joy_msg);
-
 /* ROS */
 ros::NodeHandle  nh;
 ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &messageCb);
-
-/*
-ros::Subscriber<std_msgs::Float32MultiArray> joy_sub("/joy_array", odrive_cb);
-
-std_msgs::Int32MultiArray position_data;
-ros::Publisher position_pub("/position", &position_data);
-
-std_msgs::Float32MultiArray velocity_data;
-ros::Publisher velocity_pub("/velocity", &velocity_data);
-
-std_msgs::Float32 voltage_data;
-ros::Publisher voltage_pub("/voltage", &voltage_data);
-*/
-
-/******/
 
 /***********************************************************************
  * Global variables
  **********************************************************************/
 const int kv = 16;
 const int encoder_cpr = 90;
-const float pi = 3.14159262;
+const float pi = 3.14159262f;
 
-float w_r = 0.0;
-float w_l = 0.0;
+float w_r = 0.0f;
+float w_l = 0.0f;
 
 //wheel_rad is the wheel radius ,wheel_sep is
-float wheel_rad = 0.085;
-float wheel_sep = 0.32;
+float wheel_rad = 0.085f;
+float wheel_sep = 0.32f;
 
-float speed_ang = 0.0;
-float speed_lin = 0.0;
+float speed_ang = 0.0f;
+float speed_lin = 0.0f;
 
-float vel1,vel2;
-
-/*
-void array_init(std_msgs::Int32MultiArray& data, int array){
-    data.data_length=array;
-    data.data=(int32_t *)malloc(sizeof(int32_t)*array);
-    for(int i=0; i<array ;i++){
-        data.data[i]=0;
-    }
-}
-
-void array_init(std_msgs::Float32MultiArray& data, int array){
-    data.data_length=array;
-    data.data=(float *)malloc(sizeof(float)*array);
-    for(int i=0; i<array ;i++){
-        data.data[i]=0;
-    }
-}
-*/
+float vel1 = 0.0f;
+float vel2 = 0.0f;
 
 void ros_init()
 {
-    /*
-    array_init(position_data,2);
-    array_init(velocity_data,2);
-    */
-    //nh.getHardware()->setBaud(115200);
-    nh.initNode();
-    nh.subscribe(sub);
-
-    /*
-    nh.subscribe(joy_sub);
-    nh.advertise(position_pub);
-    nh.advertise(velocity_pub);
-    nh.advertise(voltage_pub);
-    */
+  nh.getHardware()->setBaud(115200);
+  nh.initNode();
+  nh.subscribe(sub);
 }
 
 void odrive_calibration()
 {
   int motornum = 0;
   int requested_state;
+  
+  // In this example we set the same parameters to both motors.
+  // You can of course set them different if you want.
+  // See the documentation or play around in odrivetool to see the available parameters
+  //odrive_serial << "w axis" << motornum << ".controller.config.vel_limit " << 5000.0f << '\n';
+  //odrive_serial << "w axis" << motornum << ".motor.config.current_lim " << 20.0f << '\n';
 
   requested_state = ODriveArduino::AXIS_STATE_MOTOR_CALIBRATION;
   //Serial2 << "Axis" << c << ": Requesting state " << requested_state << '\n';
@@ -129,46 +98,24 @@ void odrive_calibration()
 }
 
 void setup() {
-  /*
-  odrive.odrive_reboot();
-  for(int i=0; i< 2 ; i++){
-    odrive.odrive_init(i);
-    delay(300);
-  }
-  */
-  // ODrive uses 115200 baud
-  Serial2.begin(115200);
+  // ODrive uses 115200 baud(Odriveは115200でないと動かない様子)
+  odrive_serial.begin(115200);
+  //Serial2.begin(115200);
   delay(3000);
-
-  // In this example we set the same parameters to both motors.
-  // You can of course set them different if you want.
-  // See the documentation or play around in odrivetool to see the available parameters
-  /*
-  for (int axis = 0; axis < 2; ++axis) {
-    Serial2 << "w axis" << axis << ".controller.config.vel_limit " << 5000.0f << '\n';
-    Serial2 << "w axis" << axis << ".motor.config.current_lim " << 20.0f << '\n';
-    // This ends up writing something like "w axis0.motor.config.current_lim 10.0\n"
-  }
-  */
   
+  // moterキャリブレーション
   odrive_calibration();
   delay(300);
-  
+
   ros_init();
 }
 
 void loop() {
-  /*
-  voltage_data.data=odrive.get_voltage();
-  voltage_pub.publish(&voltage_data);
-  
-  for(int motor=0; motor<2 ;motor++){
-    position_data.data[motor]=motor->pos_setpoint
-    velocity_data.data[motor]=motor->vel_setpoint
-  }
-  position_pub.publish(&position_data);
-  velocity_pub.publish(&velocity_data);
-  */
+  // loop内でSetVelocityを指定して速度制御を行う
+  vel1 = 2.0f;
+  vel2 = 2.0f;
+  odrive.SetVelocity(0, vel1);
+  odrive.SetVelocity(1, vel2);
 
   nh.spinOnce();
   delay(500);
@@ -187,13 +134,3 @@ void messageCb(const geometry_msgs::Twist& msg){
   odrive.SetVelocity(0, vel1);
   odrive.SetVelocity(1, vel2);
 }
-
-/*
-void odrive_cb(const std_msgs::Float32MultiArray& joy_msg){
-    joy_msg.data[1] > 0 ? vel1=pow((joy_msg.data[1]*3.0),2)*(40000.0/9.0) : vel1=pow((joy_msg.data[1]*3.0),2)*(-40000.0/9.0);
-    joy_msg.data[4] > 0 ? vel2=pow((joy_msg.data[4]*3.0),2)*(40000.0/9.0) : vel2=pow((joy_msg.data[4]*3.0),2)*(-40000.0/9.0);
-
-    odrive.SetVelocity(0,vel1);
-    odrive.SetVelocity(1,vel2);
-}
-*/
